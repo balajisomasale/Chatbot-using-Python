8d23586b133ea2859037ec9f6e6e43aafb39d7e2
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-05-31 22:03:24 +0530

    1st pro

---
+++ 02 Advanced Python Functions/first_three_multiples.py
@@ -0,0 +1,12 @@
+# Write your first_three_multiples function here
+
+
+# Uncomment these function calls to test your 
+
+def first_three_multiples(num):
+    print(num*3)
+    return num*3
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
