d64b15d2c1dd4a12e8b3c1caca865694cc2005d7
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-05-31 21:56:09 +0530

    5th pro

---
+++ 01 Functions Basics/remainder.py
@@ -0,0 +1,9 @@
+# Write your remainder function here:
+
+# Uncomment these function calls to test your 
+def remainder(num1,num2):
+    return (2*num1)%(0.5*num2)
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
