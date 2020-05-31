dc4f4ffe43d64b7b371a077747ddc7df7e4e84b2
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-06-01 00:25:27 +0530

    Advanced 1st challenge

---
+++ 03 Control Flows/06 in_range.py
@@ -0,0 +1,19 @@
+'''
+Create a function named in_range() that has three parameters named num, lower, and upper.
+
+The function should return True if num is greater than or equal to lower and less than or equal to upper. Otherwise, return False.'''
+
+
+
+# Write your in_range function here:
+
+# Uncomment these function calls to test your 
+def in_range(num,lower,upper):
+    if num >= lower and num <=upper:
+        return True
+    else: return False
+      
+print(in_range(10, 10, 10))
+# should print True
+print(in_range(5, 10, 20))
+# should print False
