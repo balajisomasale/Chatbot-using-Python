e8b62ccbc6b2ee6cf497479215ac3bcb6bf0ed46
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-06-02 23:24:52 +0530

    average

---
+++ 05 Python Data Stuctures/04 Dictionaries: Challenges/08 unique values.py
@@ -0,0 +1,18 @@
+'''
+  Create a function named unique_values that takes a dictionary named my_dictionary as a parameter. 
+  The function should return the number of unique values in the dictionary.
+
+
+Hint
+Begin by creating a new empty list named seen_values. Loop through all of the values of my_dictionary. 
+For every value, check to see if that value is in seen_values. If it is, continue to the next value. 
+If it is not, add it to seen_values. After looping through all values, return the length of seen_values.
+'''
+
+# Write your count_first_letter function here:
+
+# Uncomment these function calls to test your  function:
+#print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Lannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 4, "L": 3}
+#print(count_first_letter({"Stark": ["Ned", "Robb", "Sansa"], "Snow" : ["Jon"], "Sannister": ["Jaime", "Cersei", "Tywin"]}))
+# should print {"S": 7}
